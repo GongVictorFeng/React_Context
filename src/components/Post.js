970dcdc4917a7b5aed45d35976fd6b061aeac1d1
victor <gfeng005@uottawa.ca>
2025-04-01 14:23:43 -0400

    added component of post.

---
+++ src/components/Post.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Comment from "./Comment";
+
+export default function Post() {
+  return (
+    <div>
+      <h2>Example post title</h2>
+      <p>the detail of the post</p>
+      <Comment></Comment>
+    </div>
+  );
+}
